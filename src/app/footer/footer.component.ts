8478d76e3e776b45cf0e6a94345bcb637ba90922
FelipeRodriguesGare <felipe.gare@hotmail.com>
2021-11-22 23:57:24 -0300

    footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -16,28 +16,8 @@ export class FooterComponent implements OnInit {
   @Input() contactInfo = ''
   
   constructor() { }
-  
+
   ngOnInit(): void {
-    this.contactInfo ='0800-761-4631'
-    this.links = [
-    {text: 'Perguntas frequentes'},
-    {text: 'Centro de ajuda', link: ''},
-    {text: 'Conta', link: ''},
-    {text: 'Imprensa', link: ''},
-    {text: 'Relações com investidores', link: ''},
-    {text: 'Carreiras', link: ''},
-    {text: 'Resgatar cartão pré-pago', link: ''},
-    {text: 'Comprar cartão pré-pago', link: ''},
-    {text: 'Formas de assistir', link: ''},
-    {text: 'Termos de uso', link: ''},
-    {text: 'Privacidade', link: ''},
-    {text: 'Prefêrencias de cookies', link: ''},
-    {text: 'Informações corporativas', link: ''},
-    {text: 'Entre em contato', link: ''},
-    {text: 'Teste de velocidade', link: ''},
-    {text: 'Avisos legais', link: ''},
-    {text: 'Só na Netflix', link: ''}
-    ]
   }
 
 }
