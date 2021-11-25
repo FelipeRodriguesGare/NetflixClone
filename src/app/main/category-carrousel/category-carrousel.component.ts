108015edd655237a217cf52e3b91b48a00d07eec
VitorZillig <vitorgzillig@gmail.com>
2021-11-24 23:57:44 -0300

    cardCarrousel

--- src/app/main/category-carrousel/category-carrousel.component.ts
+++ src/app/main/category-carrousel/category-carrousel.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AppService, serie, seriesID } from 'src/app/app.service';
 
 @Component({
   selector: 'app-category-carrousel',
@@ -6,10 +7,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category-carrousel.component.scss']
 })
 export class CategoryCarrouselComponent implements OnInit {
+  @Input() category:string
+  @Input() isKeepWatching:boolean
 
-  constructor() { }
+  userIDs:number[] = [];
+  series:Array<serie> = [];
+  cardImage:string = ''
+
+  constructor(private appService:AppService) { }
+
+  getSeriesID(userID:number){
+    this.appService.getUserInfo(userID).subscribe(categories=>{
+     const idsArray = Object.values(categories).flat()
+      this.userIDs = idsArray
+      this.getSeries(this.userIDs)
+    })
+    
+  }
+  
+ 
+ getSeries(idsList:number[]){
+    idsList.map((id,index)=>{
+      this.appService.getSeriesInfo(id).subscribe((serie:serie)=>{
+        serie.id = id
+        serie.isKeepWatching = false
+        let serieWithID:serie =  serie;
+        let isKeepWatching:boolean = false
+
+        serieWithID.id = Number(index+1)
+
+        this.appService.getUserInfo(1).subscribe((categories:seriesID)=>{
+          categories.keepWatching.includes(serieWithID.id)?isKeepWatching = true: isKeepWatching =  false
+          serieWithID.isKeepWatching =  isKeepWatching
+        })
+        this.series.push(serieWithID)
+      })
+    })
+  }
+
 
   ngOnInit(): void {
+    
+    this.getSeriesID(1)
+    console.log(this.series)
   }
+
 
 }
